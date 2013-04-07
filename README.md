a373e2d74db398ddc788aea0ef999e714ae2d306
Gal Dubitski <galmail@gmail.com>
2013-04-07 20:58:46 +0200

    ToCarta NodeJS Project Init

---
+++ README.md
@@ -0,0 +1,44 @@
+This is a Node.js Project of ToCarta.
+
+The purpose of this project is to use Socket.io to enable Websockets and long polling
+with the Heroku platform. The reason is:
+
+1. Check tablets status
+2. Update tablets
+3. Send push messages to tablets (such as orders to the command center)
+
+
+This project publish the following API:
+
+--------------------------------
+Name: Check tablet status, version, etc.
+URI: /check/:tablet_id
+Return:
+{
+	os: 'Android',
+	os_version: '4.1.1',
+	phonegap_version: '2.5.0',
+	app_version: '1.3.1',
+	device_model: 'Samsung Galaxy Tab',
+	screen_size: '7',
+	screen_resolution: '1024x600'
+}
+Error Codes:
+	x: tablet_id is invalid
+--------------------------------
+
+--------------------------------
+Name: Update tablet
+URI: /update/:tablet_id
+Return: { return: true } if tablet received message, false otherwise.
+Error Codes:
+	x: tablet_id is invalid
+--------------------------------
+
+
+
+
+
+
+
+
---
+++ web.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var io = require('socket.io');
+
+var port = process.env.PORT || 5000;
+var app = express.createServer(express.logger());
+var io = io.listen(app);
+
+app.use(express.bodyParser());
+
+// assuming io is the Socket.IO server object
+io.configure(function () { 
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+});
+
+app.get('/', function(request, response) {
+  response.send('ToCarta NodeJS Server is up!');
+});
+
+app.get('/test', function (req, res) {
+  res.sendfile(__dirname + '/client/index.html');
+});
+
+app.get('/dist*', function (req, res) {
+	console.log('aaaaaa');
+  res.sendfile(__dirname + '/client/dist/socket.io.js');
+});
+
+
+app.get('/proxy', function (req, res) {
+	var channel = req.query["channel"];
+	var action = req.query["action"];
+	if(channel!=null && action!=null){
+		console.log("sending action: "+action+" on channel: "+channel);
+		io.sockets.emit(channel, { action: action});
+		res.send('Push notification sent');
+	}
+	else {
+		res.send('Channel and Action params are incorrect.');
+	}
+});
+
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
+io.sockets.on('connection', function (socket) {
+	console.log("Looks like someone got connected");
+	// console.log("Sending data to restaurant_1");
+  // socket.emit('restaurant_1', { update: 'yes', send_status: 'no' });
+  
+  // socket.on('my other event', function (data) {
+    // console.log(data);
+  // });
+  
+});
+
+
+
+
